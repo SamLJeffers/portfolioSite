ade4c0e2f7830e917c4ed86c908f815111636f98
SamLJeffers <88199486+SamLJeffers@users.noreply.github.com>
2022-06-13 20:05:54 +0100

    minor changes to css and layout

---
+++ php/mail.php
@@ -0,0 +1,28 @@
+<?php
+    if(isset($_POST['name']) && !empty($_POST['name']) && isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['subject']) && !empty($_POST['subject']) &&  isset($_POST['message']) && !empty($_POST['message'])){
+            $to = "samuel.jeffers@hotmail.com";
+            $subject = "My Portfolio Website | Contact Form: ".$_POST['subject'];
+
+            $message = " 
+                <b>Customer:</b>".$_POST['name']."<br>
+                <b>Email:</b>".$_POST['email']."<br>     
+                <b>Message:</b>".$_POST['message']."<br>
+                ";
+               
+// Always set content-type when sending HTML email
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+            // More headers
+            $headers .= 'From: <contactme@samueljeffers.co.uk>' . "\r\n";
+
+print $message;
+exit;
+
+            mail($to,$subject,$message,$headers);
+
+    }else{
+        print "Please fill all the required fields";
+    }
+
+?>
